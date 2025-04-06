944292c8a1eabd69d13f26d161547cc448c3a66f
acetilsalicilic <1Uncanalmas@gmail.com>
2025-04-05 23:12:22 -0600

    Made Vector2 inmutable to solve nasty problems

--- src/main/java/mathUtils/Vector2.java
+++ src/main/java/mathUtils/Vector2.java
@@ -9,17 +9,46 @@ package mathUtils;
  * @author acetil
  */
 public class Vector2 {
-    public float x;
-    public float y;
+    private float x;
+    private float y;
+    /**
+    * I had to make this structure inmutable, because I was having some
+    * nasty effects that I just couldn't pinpoint, and I was sure it had something
+    * to do with accidentally modifying a Vector2 somewhere, so I made it
+    * Inmutable and like that! problem solved. I still don't know where I was
+    * changing it, but I was it seems.
+    */
+    public float x() {
+        return x;
+    }
+    
+    public float y() {
+        return y;
+    }
+    
+    public Vector2 x(float newX) {
+        return new Vector2(newX, y);
+    }
+    
+    public Vector2 y(float newY) {
+        return new Vector2(x, newY);
+    }
     
     public Vector2(float x, float y) {
         this.x = x;
         this.y = y;
     }
     
-    public void multiplyBy(float factor) {
-        x = x * factor;
-        y = y* factor;
+    public Vector2 multiplyBy(float factor) {
+        return new Vector2(x * factor, y * factor);
+    }
+    
+    public Vector2 sum(Vector2 value) {
+        return new Vector2(x + value.x, y + value.y);
+    }
+    
+    public Vector2 sum(float value) {
+        return new Vector2(x + value, y + value);
     }
     
     public float[] toArray() {
