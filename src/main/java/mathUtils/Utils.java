8feabb10de8449a8748f87f5a353270ba0fbe23a
acetilsalicilic <1Uncanalmas@gmail.com>
2025-04-05 23:13:04 -0600

    A simple (and silly) function to determine the sign of a number

---
+++ src/main/java/mathUtils/Utils.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mathUtils;
+
+/**
+ *
+ * @author acetil
+ */
+public class Utils {
+    public static int getSign(float number) {
+        if (number < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
