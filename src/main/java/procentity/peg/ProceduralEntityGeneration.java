fd54b702b00e7578ceb9a3e5935b0240891c41de
acetilsalicilic <1Uncanalmas@gmail.com>
2025-04-05 23:17:55 -0600

    Changes for trying things. I really should use JUnit for this...

--- src/main/java/procentity/peg/ProceduralEntityGeneration.java
+++ src/main/java/procentity/peg/ProceduralEntityGeneration.java
@@ -9,6 +9,7 @@ import mathUtils.Vector2;
 import movement.Player;
 import world.World;
 import world.terrain.TerrainGenerator;
+import worldprinting.WorldPrinter;
 
 /**
  *
@@ -26,13 +27,19 @@ public class ProceduralEntityGeneration {
         logger.log("World: " + world);
         
         // confirming player
-        Player.initialize(new Vector2(0.5f, 0.5f));
+        Player.initialize(new Vector2(-14f, 14f));
         logger.log("Player: " + Player.getInstance());
         logger.log("Creating terrain generator");
-        TerrainGenerator tr = new TerrainGenerator(5f, 3, true, false, false, true);
+        TerrainGenerator tr = new TerrainGenerator(7f, 3, true, true, false, true);
         
         logger.log("First analyzing");
         tr.evaluatePlayer(Player.getInstance(), world);
+        
+        logger.log("Second analysis");
+        tr.evaluatePlayer(Player.getInstance(), world);
+        
+        WorldPrinter wp = new WorldPrinter(world);
+        wp.printChunks();
         // Game loop
         /*while (true) {
             
